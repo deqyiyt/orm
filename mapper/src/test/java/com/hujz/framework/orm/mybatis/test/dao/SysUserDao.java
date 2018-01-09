7db21dc7eaefccedddd7007ed955ab7e0aefde6e
jiuzhou.hu <352deqyiyt@163.com>
2018-01-09 19:21:36 +0800

    清除

--- mapper/src/test/java/com/hujz/framework/orm/mybatis/test/dao/SysUserDao.java
+++
@@ -1,9 +0,0 @@
-package com.hujz.framework.orm.mybatis.test.dao;
-
-import com.hujz.framework.orm.GenericDao;
-import com.hujz.framework.orm.mybatis.test.entity.SystemUser;
-
-public interface SysUserDao extends GenericDao<SystemUser, String>{
-	
-	SystemUser selectById(String id);
-}
